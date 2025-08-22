2aa195a0243646baffb85b1dcded5ae54d59a4f7
Andrej Urvantsev <andrej.urvantsev@postnord.com>
2025-08-22 15:31:18 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,20 +3,20 @@ plugins {
     id("io.spring.dependency-management") version "1.1.7"
 
     // https://docs.spring.io/spring-boot/gradle-plugin/index.html
-    id("org.springframework.boot") version "3.4.1"
+    id("org.springframework.boot") version "3.5.5"
 
     // https://github.com/spotbugs/spotbugs-gradle-plugin
-    id("com.github.spotbugs") version "6.0.27"
+    id("com.github.spotbugs") version "6.2.5"
 
     // https://github.com/diffplug/spotless/tree/main/plugin-gradle
-    id("com.diffplug.spotless") version "6.25.0"
+    id("com.diffplug.spotless") version "7.2.1"
 
     // https://github.com/ben-manes/gradle-versions-plugin
-    id("com.github.ben-manes.versions") version "0.51.0"
+    id("com.github.ben-manes.versions") version "0.52.0"
 
     // https://github.com/graalvm/native-build-tools
     // https://graalvm.github.io/native-build-tools/latest/gradle-plugin.html
-    id("org.graalvm.buildtools.native") version "0.10.4"
+    id("org.graalvm.buildtools.native") version "0.11.0"
 
     checkstyle
     jacoco
@@ -50,35 +50,35 @@ dependencies {
     // https://github.com/KengoTODA/findbugs-slf4j
     spotbugsPlugins("jp.skypencil.findbugs.slf4j:bug-pattern:1.5.0@jar")
     // https://github.com/mebigfatguy/fb-contrib/releases/latest
-    spotbugsPlugins("com.mebigfatguy.sb-contrib:sb-contrib:7.6.9")
+    spotbugsPlugins("com.mebigfatguy.sb-contrib:sb-contrib:7.6.13")
     // https://github.com/find-sec-bugs/find-sec-bugs/
-    spotbugsPlugins("com.h3xstream.findsecbugs:findsecbugs-plugin:1.13.0")
+    spotbugsPlugins("com.h3xstream.findsecbugs:findsecbugs-plugin:1.14.0")
 
     // https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#configuration-metadata-annotation-processor
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     // https://docs.spring.io/spring/docs/current/spring-framework-reference/core.html#beans-scanning-index
     annotationProcessor("org.springframework:spring-context-indexer")
 
-    implementation("com.formdev:flatlaf:3.5.4")
+    implementation("com.formdev:flatlaf:3.6.1")
     implementation("org.springframework.boot:spring-boot-starter")
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:okhttp:5.1.0")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api")
     testImplementation("org.junit.jupiter:junit-jupiter-params")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 
     // https://assertj.github.io/doc/#assertj-overview
-    testImplementation("org.assertj:assertj-core:3.27.0")
+    testImplementation("org.assertj:assertj-core:3.27.4")
 
     // https://github.com/datafaker-net/datafaker/
-    testImplementation("net.datafaker:datafaker:2.4.2")
+    testImplementation("net.datafaker:datafaker:2.4.4")
 }
 
 spotless {
     java {
         target("src/main/java", "src/test/java")
         // https://github.com/palantir/palantir-java-format/releases
-        palantirJavaFormat("2.50.0")
+        palantirJavaFormat("2.73.0")
     }
 }
 
@@ -175,7 +175,7 @@ graalvmNative {
     metadataRepository {
         enabled.set(true)
         // https://github.com/oracle/graalvm-reachability-metadata/releases
-        version.set("0.3.15")
+        version.set("0.3.24")
     }
     binaries {
         named("main") {
